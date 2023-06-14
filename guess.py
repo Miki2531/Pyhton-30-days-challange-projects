f41a13e10b6c6acaa85aaaeec09bbe0ba3fc4acb
Miki2531 <mikiyasshemelis46@gmail.com>
2023-06-14 13:09:44 +0300

    evevn or odd

--- guess.py
+++ guess.py
@@ -1,32 +1,43 @@
 from random import shuffle
 
 
-mylist = [' ', 'O', ' ']
+# mylist = [' ', 'O', ' ']
 
 
-def shuffle_list(mylist):
-    shuffle(mylist)
-    return mylist
+# def shuffle_list(mylist):
+#     shuffle(mylist)
+#     return mylist
 
 
-def guess_player():
-    guess = ''
-    while guess not in ['0', '1', '2']:
-        guess = input('Pick the number: 0, 1 or 2 ')
+# def guess_player():
+#     guess = ''
+#     while guess not in ['0', '1', '2']:
+#         guess = input('Pick the number: 0, 1 or 2 ')
 
-    return int(guess)
-
-
-# guess_player()
+#     return int(guess)
 
 
-def check_guess(mylist, guess):
-    if mylist[guess] == 'O':
-        print("Correct!")
+# # guess_player()
+
+
+# def check_guess(mylist, guess):
+#     if mylist[guess] == 'O':
+#         print("Correct!")
+#     else:
+#         print("Wrong guess!")
+
+
+# mixedup_list = shuffle_list(mylist)
+# guess = guess_player()
+# check_guess(mixedup_list, guess)
+
+
+def lesser_of_two_evens(a, b):
+    if a % 2 == 0 and b % 2 == 0:
+        return min(a, b)
     else:
-        print("Wrong guess!")
+        return max(a, b)
 
 
-mixedup_list = shuffle_list(mylist)
-guess = guess_player()
-check_guess(mixedup_list, guess)
+print(lesser_of_two_evens(2, 4))
+print(lesser_of_two_evens(2, 5))
