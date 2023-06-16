412352526e37c4ee8a821535848773fcfdcd4d80
Miki2531 <mikiyasshemelis46@gmail.com>
2023-06-16 16:16:43 +0300

    'game'

--- tic_game.py
+++ tic_game.py
@@ -6,9 +6,6 @@ def display_game(game_list):
     print(game_list)
 
 
-display_game(game_list)
-
-
 def position_choice():
     choice = 'WRONG'
 
@@ -20,14 +17,31 @@ def position_choice():
     return int(choice)
 
 
-print(position_choice())
-
-
-def replacement_position(game_list, position):
+def replacement_choice(game_list, position):
     user_replacement = input("Inter the replacemnent value: ")
     game_list[position] = user_replacement
 
     return game_list
 
 
-print(replacement_position(game_list, 1))
+def gameon_choice():
+    choice = 'WRONG'
+
+    while choice not in ['Y', 'N']:
+        choice = input("Keep playing the game: ")
+        if choice not in ['Y', 'N']:
+            print("Sorry, I dont understand, please valid choice Y or N")
+
+    if choice == "Y":
+        return True
+    else:
+        return False
+
+
+game_on = True
+while game_on:
+    display_game(game_list)
+    position = position_choice()
+    replace_value = replacement_choice(game_list, position)
+    display_game(game_list)
+    game_on = gameon_choice()
